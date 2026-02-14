10e648646e35c425a36a3afb73d6282a9e1e5c1e
Shayan Arman <shayan.arman2@gmail.com>
2026-02-14 11:18:53 -0800

    update: form

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+'use client'
+import { useForm, ValidationError } from '@formspree/react';
 import { FaInstagram, FaFacebook } from "react-icons/fa";
 
 export default function Home() {
@@ -407,5 +409,41 @@ function ContactInfo({ label, value, href }: { label: string; value: string; hre
         {value}
       </a>
     </div>
+  );
+}
+
+function ContactForm() {
+  const [state, handleSubmit] = useForm("mvzbaokq");
+  if (state.succeeded) {
+    return <p>Thanks for joining!</p>;
+  }
+  return (
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="email">
+        Email Address
+      </label>
+      <input
+        id="email"
+        type="email"
+        name="email"
+      />
+      <ValidationError
+        prefix="Email"
+        field="email"
+        errors={state.errors}
+      />
+      <textarea
+        id="message"
+        name="message"
+      />
+      <ValidationError
+        prefix="Message"
+        field="message"
+        errors={state.errors}
+      />
+      <button type="submit" disabled={state.submitting}>
+        Submit
+      </button>
+    </form>
   );
 }
